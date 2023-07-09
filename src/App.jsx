2f42aecb7c49e3b43500e88068aa47b27552fa26
mukesh <mukeshmehta2041@gmail.com>
2023-07-09 14:08:19 +0530

    starting the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  return (
+    <div className="App">
+      <h1>News Api</h1>
+    </div>
+  );
+}
+
+export default App;
