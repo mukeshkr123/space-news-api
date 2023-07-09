61301b061c1c04904fad186630f3ea888445601c
mukesh <mukeshmehta2041@gmail.com>
2023-07-09 14:15:24 +0530

    fetching the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [newsList, setNewsList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.spaceflightnewsapi.net/v4/articles/")
+      .then((response) => response.json())
+      .then((data) => {
+        setNewsList(data);
+        console.log(data.results);
+      })
+      .catch((error) => console.log(error));
+  });
 
   return (
     <div className="App">
       <h1>News Api</h1>
+
+      <div className="news-container"></div>
     </div>
   );
 }
